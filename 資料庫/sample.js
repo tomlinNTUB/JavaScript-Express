5a069ecf42be23db15b5520f34d1c524a7b0d12b
林宏仁 <tom.hr.lin@gmail.com>
2018-05-05 13:06:11 +0800

    Add files via upload

---
+++ 資料庫/sample.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+//----------------------------
+// 引用db.js的pool物件
+//----------------------------
+var pool = require('./db.js');
+
+
+//========================
+// 處理GET請求
+//========================
+router.get('/', function(req, res, next) {
+    pool.query('select * from product', function (error, results, fields) {
+        if (error||results.length==0){
+            res.render('dataNotFound', {});  //轉給找不到資料的畫面
+        }else{
+            res.render('productList', {items:results});  //轉給顯示資料的畫面
+        }       
+    });
+});
+
+module.exports = router;
