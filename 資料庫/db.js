51bd386f77c02f15d270d92e26401f2e53745c10
林宏仁 <tom.hr.lin@gmail.com>
2018-05-05 12:58:13 +0800

    Add files via upload

---
+++ 資料庫/db.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+//------------------------
+// 建立資料庫連線池
+//------------------------
+var pool  = mysql.createPool({
+    user: 'root',
+    password: 'mysql',
+    host: '127.0.0.1',
+    database: 'north', 
+    waitForConnections : true, 
+    connectionLimit : 10       
+});
+
+//----------------------------
+// 引用此模組時將匯出pool物件
+//----------------------------
+module.exports = pool;
