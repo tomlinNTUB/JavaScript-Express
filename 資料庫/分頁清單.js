8011e7bc16f73a20a61238694d4234b8f5a170c6
林宏仁 <tom.hr.lin@gmail.com>
2018-05-09 13:15:45 +0800

    Add files via upload

---
+++ 資料庫/分頁清單.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+//----------------------------------------------------
+// 透過require引用db.js的pool物件,
+// 即使多個程式均引用, 在系統中只有一份pool物件.
+//----------------------------------------------------
+var pool = require('./db.js');
+
+var startPage=1;
+var linePerPage=15; 
+var navSegments=10;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var pageNo=parseInt(req.param('pageNo'));
+
+    //--------------------------
+    // 如果輸入參數非數字
+    //--------------------------
+    if(isNaN(pageNo)){
+        pageNo=1;
+    }
+
+    //--------------------------
+    // 如果輸入參數小於1
+    //--------------------------
+    if(pageNo<1){
+        pageNo=1;
+    }
+
+    //-----------------------
+    // 如果點了上一個區段
+    //-----------------------
+    if(pageNo<startPage){
+        startPage=startPage-navSegments;
+    }
+
+    //-----------------------
+    // 如果點了下一個區段
+    //-----------------------   
+    if(pageNo>=(startPage+navSegments)){
+        startPage=startPage+navSegments;
+    }
+
+    pool.query('select count(*) as cnt from product', function(err, results) {
+        if (err)throw err;
+
+        var totalLine=results[0].cnt;
+        var totalPage=Math.ceil(totalLine/linePerPage);
+
+        pool.query('select * from product limit ?, ?',[(pageNo-1)*linePerPage, linePerPage], function(err, results) {
+            if (err) {
+                res.render('dataNotFound', {});
+            }
+
+            if(results.length==0){
+                res.render('dataNotFound', {});
+            }else{
+                var recordNo=(pageNo-1)*linePerPage+1;
+                res.render('product', {data:results, pageNo:pageNo, totalLine:totalLine, totalPage:totalPage, startPage:startPage, linePerPage:linePerPage, navSegments:navSegments});
+            }
+        }); 
+    }); 
+});
+
+module.exports = router;
