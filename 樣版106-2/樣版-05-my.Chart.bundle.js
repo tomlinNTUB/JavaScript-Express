5f566ed84654f365bfdcc78ffbb2022cdaf71727
林宏仁 <tom.hr.lin@gmail.com>
2018-05-10 22:46:58 +0800

    Update 樣版-05-my.Chart.bundle.js

--- 樣版106-2/樣版-05-my.Chart.bundle.js
+++ 樣版106-2/樣版-05-my.Chart.bundle.js
@@ -3,10 +3,10 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: label,
+        labels: myLabel,
         datasets: [{
             label: '產品價格',
-            data: data,
+            data: myData,
             fill: false,
             backgroundColor: 'rgba(212, 106, 106, 1)',
             borderColor: 'rgba(212, 106, 106, 1)'
