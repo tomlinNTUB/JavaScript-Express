e3a654985239b62c4c41bb7e13069c1c6c0d4728
林宏仁 <tom.hr.lin@gmail.com>
2018-05-28 13:13:30 +0800

    Create 13-程式-登入.js

---
+++ 樣版106-2/13-程式-登入.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+//------------------
+// 載入資料庫連結
+//------------------
+var pool = require('./db.js');
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+    //取得使用者傳來的參數
+    var userid=req.param("userid");
+    var password=req.param("password");
+
+    pool.query('select * from employee where empNo=? and password=?', [userid, password], function(err, rows, fields) {
+        if (err || rows.length==0){ //登入失敗
+            req.session.loginPass=false;
+            req.session.userid=''; 
+			req.session.username=null; 			            
+			res.render('index', {username:'登入失敗'});  
+        }else{	//登入成功
+            req.session.loginPass=true;
+            req.session.userid=userid; 	
+            req.session.username=rows[0].empName; 	
+            res.render('index', {username:req.session.username});   
+        }
+    });
+});
+
+module.exports = router;
+
+
