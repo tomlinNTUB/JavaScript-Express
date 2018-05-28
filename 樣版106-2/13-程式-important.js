ab08aaffaf9d1db927a161ef002950d1cc5d18ec
林宏仁 <tom.hr.lin@gmail.com>
2018-05-28 13:15:36 +0800

    Create 13-程式-important.js

---
+++ 樣版106-2/13-程式-important.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+//----------------------------------------------
+// 載入使用權檢查
+//----------------------------------------------
+var authorize = require('./authorize.js');
+//----------------------------------------------
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {	
+    //------------------------------------------
+    // 如尚未登入, 轉至未登入頁面
+    //------------------------------------------
+    if(!authorize.isPass(req)){
+        res.render('NOTloginned', {});
+        return;
+    }
+    //------------------------------------------
+
+
+	//已登入使用者	
+    res.render('important', {userid:req.session.userid, username:req.session.username});
+});
+
+module.exports = router;
+
+
