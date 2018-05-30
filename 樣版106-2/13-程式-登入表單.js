11d90b5a2f66be317a4548dd53baa8b40a815173
林宏仁 <tom.hr.lin@gmail.com>
2018-05-30 14:12:59 +0800

    Update 13-程式-登入表單.js

--- 樣版106-2/13-程式-登入表單.js
+++ 樣版106-2/13-程式-登入表單.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var username = req.session.username;
-	if(username==null||username==""){
-		username='尚未登入';
-	}
+    var username = req.session.username;
+    if(username==null || username==''){
+        username='尚未登入';
+    }
 		
-	res.render('loginForm', {username:username});
+    res.render('loginForm', {username:username});
 });
 
 module.exports = router;
