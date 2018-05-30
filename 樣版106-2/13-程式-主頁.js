8e26c2fdde96a7fe204057f9a20dd97cfabcb572
林宏仁 <tom.hr.lin@gmail.com>
2018-05-30 14:11:41 +0800

    Update 13-程式-主頁.js

--- 樣版106-2/13-程式-主頁.js
+++ 樣版106-2/13-程式-主頁.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var username = req.session.username;
-	if(username==null||username==""){
-		username='尚未登入';
-	}
+    var username = req.session.username;
+    if(username==null||username==""){
+        username='尚未登入';
+    }
 	
-	res.render('index', {username:username});
+    res.render('index', {username:username});
 });
 
 module.exports = router;
