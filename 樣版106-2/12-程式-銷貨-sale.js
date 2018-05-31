5fe862a072ce41fe4fb474152e5d8dbf24225931
林宏仁 <tom.hr.lin@gmail.com>
2018-05-31 11:43:33 +0800

    Update 12-程式-銷貨-sale.js

--- 樣版106-2/12-程式-銷貨-sale.js
+++ 樣版106-2/12-程式-銷貨-sale.js
@@ -44,31 +44,35 @@ router.post('/', function(req, res, next) {
                     ordDate:ordDate,
                     transFee:transFee
                 }	
-				
+ 
+                //=============================
+                // 第 1 個資料庫動作		
+                //=============================	
                 connection.query("INSERT INTO ordMaster SET ?", newMaster, function(err, results, fields) {
-                    //======================
-                    // 交易失敗, Rollback
-                    //======================				
+                    //-------------------------
+                    // 如果交易失敗, Rollback
+                    //-------------------------		
                     if (err) {
                         connection.rollback(function() {
                             connection.release();
                             res.render('transactionRollback', {});
                         });
                     } else {	
-                        //增加交易明細記錄
                         var tot=0;
                         for(var i=1; i<proNo.length; i++){
-                            //建立一個明細訂單資料物件
                             var newDetails={
                                 ordNo:ordNo,
                                 proNo:proNo[i],
                                 amt:amt[i]							
                             }				
-						
+				
+                            //=============================
+                            // 第 2 個資料庫動作		
+                            //=============================				    
                             connection.query("INSERT INTO ordDetails SET ?", newDetails, function(err, results, fields) {
-                                //======================
-                                // 交易失敗, Rollback
-                                //======================				
+                                //-------------------------
+                                // 如果交易失敗, Rollback
+                                //-------------------------					
                                 if (err) {
                                     connection.rollback(function() {
                                         connection.release();
@@ -76,9 +80,9 @@ router.post('/', function(req, res, next) {
                                     });	
                                 } else {
                                     tot=tot+1;
-                                    //======================
+                                    //------------------------
                                     // 交易成功, Commit
-                                    //======================					
+                                    //------------------------		
                                     if(tot==proNo.length-1){
                                         connection.commit(function(err) {
                                             if (err) {
